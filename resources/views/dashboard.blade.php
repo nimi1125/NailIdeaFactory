f452c3181b44a2b2d320e5d397839f987e2cdf22
nimi1125 <sikaku1111@gmail.com>
2024-11-11 22:52:55 +0900

    update/add-画像追加

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,41 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            <h3 class="titH3 pattaya">Nail idea <span class="text-sm">ネイルアイディア</span></h3>
+            <div class="flex flex-wrap">
+                <div class="md:w-1/2 w-full p-2">
+                    <a class="flex flex-col items-center bg-white rounded p-3.5">
+                        <img src="{{ asset('img/quill_pen_icon.png') }}" alt="羽ペン" class="mx-auto">
+                        <p class="text-2xl mt-2 pattaya">New registration</p>
+                        <p class="text-l">新規登録</p>
+                    </a>
+                </div>
+                <div class="md:w-1/2 w-full p-2">
+                    <a class="flex flex-col items-center bg-white rounded p-3.5">
+                        <img src="{{ asset('img/note_icon.png') }}" alt="ノート" class="mx-auto">
+                        <p class="text-2xl mt-2 pattaya">List display</p>
+                        <p class="text-l">一覧表示</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="dashPostArea relative max-w-6xl mx-auto sm:px-6 lg:px-8 mt-5">
+            <h3 class="titH3 pattaya">Nail Post <span class="text-sm">ネイル投稿</span></h3>
+            <p class="text-2xl">※準備中</p>
+            <div class="flex flex-wrap">
+                <div class="md:w-1/2 w-full p-2">
+                    <a class="flex flex-col items-center bg-white rounded p-3.5">
+                        <img src="{{ asset('img/quill_pen_icon.png') }}" alt="羽ペン" class="mx-auto">
+                        <p class="text-2xl mt-2 pattaya">New registration</p>
+                        <p class="text-l">新規登録</p>
+                    </a>
+                </div>
+                <div class="md:w-1/2 w-full p-2">
+                    <a class="flex flex-col items-center bg-white rounded p-3.5">
+                        <img src="{{ asset('img/note_icon.png') }}" alt="ノート" class="mx-auto">
+                        <p class="text-2xl mt-2 pattaya">List display</p>
+                        <p class="text-l">一覧表示</p>
+                    </a>
                 </div>
             </div>
         </div>
