d4f387b553cb0bab57bf31974031763802fb7dda
nimi1125 <sikaku1111@gmail.com>
2024-11-24 22:44:57 +0900

    update

--- .vercel/build.sh
+++ .vercel/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install necessary packages
-apt-get update && apt-get install -y libssl1.0.0
+apt-get update && apt-get install -y libssl1.0-dev
 
 # Continue with the usual build process
 vercel build
