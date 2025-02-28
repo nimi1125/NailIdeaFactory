460c577237fc1b46550e00d101d08ca079bbaf07
nimi1125 <sikaku1111@gmail.com>
2025-02-28 14:32:32 +0900

    add server.js

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-import express from 'express';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
+const express = require('express');
+const path = require('path');
 
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 // `dist` フォルダを静的ファイルとして提供
-app.use(express.static(join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // すべてのリクエストを `index.html` にリダイレクト
 app.get('*', (req, res) => {
-  res.sendFile(join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 // Heroku のポートを使用
